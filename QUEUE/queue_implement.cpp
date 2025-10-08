8c3eccb2282b38b081a7f799f75740827c1d9afa
mujeebarshad007 <mujeeb.arshad001@gmail.com>
2025-10-08 11:48:31 +0500

    Lab task 08 oct 2025 Queue

--- QUEUE/queue_implement.cpp
+++ QUEUE/queue_implement.cpp
@@ -2,18 +2,16 @@
 #include "header.h"
 int main()
 {
-    std::cout << "Welcome to Lab Task 08 October 2025" << std::endl;
+    std::cout << " Welcome to Lab Task 08 October 2025" << std::endl;
     queue q;
     q.push(10); // Pushing The Values
     q.push(20);
+    q.push(60);
+    q.push(78);
+    q.push(65);
     q.push(39);
 
-    while (!q.empty())
-    {
-        std::cout << q.front() << std :: << endl;
-        q.pop();
-    }
-
+    std::cout << " The Values Pushed Are 10 20 60 78 65 39 \n";
     try
     {
         std::cout << " The Front Value Of this Queue is :" << q.front() << std::endl;
@@ -25,12 +23,15 @@ int main()
 
     try
     {
-        std::cout << "The Back Value Of this Queue is :" << q.back() << std ::endl;
+        std::cout << " The Back Value Of this Queue is :" << q.back() << std ::endl;
     }
     catch (const char *msg)
     {
         std::cout << msg << std::endl;
     }
+
+    // Size OF QUEUE
+    std::cout << " The  Size of this Queue is " << q.size() << std::endl;
 
     // Popping The Value From the Queue
     q.pop();
@@ -50,15 +51,15 @@ int main()
 
     try
     {
-        std::cout << "The Back Value Of this Queue is :" << q.back() << std ::endl;
+        std::cout << " The Back Value Of this Queue is :" << q.back() << std ::endl;
     }
     catch (const char *msg)
     {
         std::cout << msg << std::endl;
     }
 
-    // Size OF QUEUE
-    std::cout << " The Size of this Queue is " << q.size() << std::endl;
+    // Updated Size OF QUEUE
+    std::cout << " The Updated Size of this Queue is " << q.size() << std::endl;
 
     return 0;
 }
