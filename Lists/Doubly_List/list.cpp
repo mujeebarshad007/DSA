0ebe44e3a0deac772dd5590b179e0f8cb3cef04d
mujeebarshad007 <mujeeb.arshad001@gmail.com>
2025-11-03 00:02:54 +0500

    Reverese_iterator_for double Linked list

--- Lists/Doubly_List/list.cpp
+++ Lists/Doubly_List/list.cpp
@@ -3,15 +3,31 @@
 int main()
 {
     list<int> l;
-    l.push_front(1);
-    l.push_front(2);
-    l.push_front(3);
-    list<int>::iterator it;
-    it = l.begin();
-    while (it != l.end())
+    try
     {
-        std::cout << *it << " ";
-        ++it;
+        l.push_front(1);
+        l.push_front(2);
+        l.push_front(3);
     }
+    catch (const char *msg)
+    {
+        std::cout << msg;
+    }
+    list<int>::iterator it;
+    // it = l.begin();
+    // while (it != l.end())
+    // {
+    //     std::cout << *it << " ";
+    //     ++it;
+    // }
+    list<int>::reverse_iterator r_it;
+    r_it = l.r_begin();
+    while (r_it != l.r_end())
+
+    {
+        std::cout << *r_it << " ";
+        ++r_it;
+    }
+    std ::cout << std::endl;
     return 0;
 }
--- Lists/Doubly_List/list.h
+++ Lists/Doubly_List/list.h
@@ -32,62 +32,62 @@ public:
     {
         return n;
     }
-    void clear()
-    {
-        while (!empty)
-        {
-            pop.front();
-        }
-    }
+    // void clear()
+    // {
+    //     while (!empty)
+    //     {
+    //         pop.front();
+    //     }
+    // }
 
     void push_front(const T &val)
     {
-        if (!empty())
-        {
+        // if (!empty())
+        // {
 
-            dnode<T> *nn;
-            nn = new dnode<T>;
-            nn->value = val;
-            nn->next = H->next;
-            nn->prev = H;
-            H->next = nn;
-            nn->next->prev = nn;
-            ++n;
-        }
-        else
-            throw "list is Empty"
+        dnode<T> *nn;
+        nn = new dnode<T>;
+        nn->value = val;
+        nn->next = H->next;
+        nn->prev = H;
+        H->next = nn;
+        nn->next->prev = nn;
+        ++n;
+        // else
+        //     throw "list is Empty";
     }
 
     void push_back(const T &val)
     {
-        if (!empty())
-        {
-            dnode<T> *nn;
-            nn = new dnode<T>;
-            nn->value = val;
-            nn->next = H;
-            nn->prev = H->prev;
-            H->prev = nn;
-            nn->prev->next = nn;
-            ++n;
-        }
-        else
-            throw "list is Empty"
+        // if (!empty())
+        // {
+        dnode<T> *nn;
+        nn = new dnode<T>;
+        nn->value = val;
+        nn->next = H;
+        nn->prev = H->prev;
+        H->prev = nn;
+        nn->prev->next = nn;
+        ++n;
+        // else
+        //     throw "list is Empty";
     }
 
     void pop_front()
     {
-        if (!empty())
-        {
-            dnode<T> *temp;
-            temp = H->next;
-            H->next = temp->next;
-            temp->next->prev = H;
-            delete temp;
-            --n;
-        }
-        else
-            throw "list is Empty"
+        // if (!empty())
+        // {
+        dnode<T> *temp;
+        temp = H->next;
+        H->next = temp->next;
+        temp->next->prev = H;
+        delete temp;
+        --n;
+        // }
+        // else
+        // {
+        // throw "list is Empty";
+        // }
     }
     void pop_back()
     {
@@ -106,7 +106,7 @@ public:
     {
     private:
         dnode<T> *ptr;
-        template <typename T>
+        template <typename U>
         friend class list;
 
     public:
@@ -145,12 +145,40 @@ public:
             return temp;
         }
 
-        // bool operator!=(const iterator &rhs) const
-        // {
-        //     return this->ptr != rhs.ptr;
-        // }
-
         bool operator!=(const iterator &rhs) const
+        {
+            return this->ptr != rhs.ptr;
+        }
+    };
+    class reverse_iterator
+    {
+    private:
+        dnode<T> *ptr;
+        template <typename U>
+        friend class list;
+
+    public:
+        T &operator*() const
+        {
+            return ptr->value;
+        }
+        reverse_iterator &operator++()
+        {
+            if (ptr != nullptr)
+                ptr = ptr->prev;
+            return *this;
+        }
+
+        reverse_iterator &operator++(int)
+        {
+            reverse_iterator temp;
+            temp = *this;
+            if (ptr != nullptr)
+                ptr = ptr->prev;
+            return temp;
+        }
+
+        bool operator!=(const reverse_iterator &rhs) const
         {
             return this->ptr != rhs.ptr;
         }
@@ -159,7 +187,7 @@ public:
     iterator insert(iterator pos, const T &val)
     {
         dnode<T> *nn;
-        nn = new node<T>;
+        nn = new dnode<T>;
         nn->value = val;
         nn->next = pos.ptr;
         nn->prev = pos.ptr->prev;
@@ -177,7 +205,31 @@ public:
     iterator end()
     {
         iterator it;
+        it.ptr = H;
+        return it;
+    }
+    iterator rbegin()
+    {
+        iterator it;
         it.ptr = H->prev;
+        return it;
+    }
+    iterator rend()
+    {
+        reverse_iterator it;
+        it.ptr = H->next;
+        return it;
+    }
+    reverse_iterator r_begin()
+    {
+        reverse_iterator it;
+        it.ptr = H->prev;
+        return it;
+    }
+    reverse_iterator r_end()
+    {
+        reverse_iterator it;
+        it.ptr = H;
         return it;
     }
     // list<T> &operator=(const list<T> &rhs)
