5930cd37c77157bd7ab8b4b477cfbb8f83dce216
mujeebarshad007 <mujeeb.arshad001@gmail.com>
2025-11-05 17:53:49 +0500

    LAB BST

--- BST/client.cpp
+++ BST/client.cpp
@@ -1,16 +1,65 @@
 #include <iostream>
-#include "tnode.h"
 #include "set.h"
 
 int main()
 {
-    set<int> s; // create a set of integers
+    set<int> s;
 
+    s.insert(10);
     s.insert(5);
-    s.insert(2);
-    s.insert(1);
-    s.insert(6);
-    s.insert(11);
+    s.insert(15);
+    s.insert(12);
 
-    std::cout << "Inserted 5, 2, 1, 6, 11 successfully!" << std::endl;
+    std::cout << "Size: " << s.size() << "\n";
+
+    if (!s.empty())
+    {
+        std::cout << "Set is not empty\n";
+    }
+
+    if (s.contains(10))
+        std::cout << "Contains 10? Yes\n";
+    else
+        std::cout << "Contains 10? No\n";
+
+    if (s.contains(7))
+        std::cout << "Contains 7? Yes\n";
+    else
+        std::cout << "Contains 7? No\n";
+
+    // Using count
+    std::cout << "Count 15: " << s.count(15) << "\n"; // 1
+    std::cout << "Count 8: " << s.count(8) << "\n";   // 0
+
+    // Iterating using iterator
+    std::cout << "Elements in set (in-order): ";
+
+    for (set<int>::iterator it = s.begin(); it != s.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << "\n";
+
+    // Using find
+    auto it = s.find(12);
+    if (it != s.end())
+    {
+        std::cout << "Found 12 using find: " << *it << "\n";
+    }
+    else
+    {
+        std::cout << "12 not found\n";
+    }
+
+    // Trying to insert duplicate
+    try
+    {
+        s.insert(10);
+    }
+    catch (const char *msg)
+    {
+        std::cout << "Error: " << msg << "\n"; // Should print duplicate error
+    }
+
+    return 0;
 }
--- BST/set.h
+++ BST/set.h
@@ -1,5 +1,4 @@
 #include "tnode.h"
-
 template <typename key_type>
 class set
 {
@@ -16,6 +15,81 @@ public:
         H->is_nill = true;
         n = 0;
     }
+
+    class iterator
+    {
+    private:
+        tnode<key_type> *node;
+        tnode<key_type> *H;
+        template <typename U>
+        friend class set;
+
+    public:
+        iterator(tnode<key_type> *n, tnode<key_type> *h)
+        {
+            node = n;
+            H = h;
+        }
+
+        key_type &operator*()
+        {
+            return node->key;
+        }
+
+        iterator &operator++()
+        {
+            if (node->right != H)
+            {
+                node = node->right;
+                while (node->left != H)
+                    node = node->left;
+            }
+            else
+            {
+                tnode<key_type> *parent = node->parent;
+                while (parent != H && node == parent->right)
+                {
+                    node = parent;
+                    parent = parent->parent;
+                }
+                node = parent;
+            }
+            return *this;
+        }
+
+        bool operator!=(const iterator &other) const
+        {
+            return node != other.node;
+        }
+        bool operator==(const iterator &it) const
+        {
+            return node == it.node;
+        }
+        key_type *operator->() { return &(node->key); }
+    };
+    iterator begin()
+    {
+        return iterator(H->left, H);
+    }
+    iterator end()
+    {
+        return iterator(H, H);
+    }
+    iterator find(const key_type &key)
+    {
+        tnode<key_type> *temp = H->parent;
+        while (temp != H)
+        {
+            if (key < temp->key)
+                temp = temp->left;
+            else if (key > temp->key)
+                temp = temp->right;
+            else
+                return iterator(temp, H);
+        }
+        return end();
+    }
+
     void insert(const key_type &key)
     {
         tnode<key_type> *nn, *temp;
@@ -79,5 +153,58 @@ public:
             H->right = nn;
         }
         ++n;
+    }
+    bool empty() const
+    {
+        return (H->parent == H);
+    }
+    int size()
+    {
+        return n;
+    }
+
+    int count(const key_type &key) const // checks if any duplicate is there or not
+    {
+
+        tnode<key_type> *temp;
+        temp = H->parent;
+        while (temp != H)
+        {
+            if (key < temp->key)
+            {
+                temp = temp->left;
+            }
+            else if (key > temp->key)
+            {
+                temp = temp->right;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        return 0;
+    }
+    bool contains(const key_type &key) const // checks if any duplicate is there or not
+    {
+
+        tnode<key_type> *temp;
+        temp = H->parent;
+        while (temp != H)
+        {
+            if (key < temp->key)
+            {
+                temp = temp->left;
+            }
+            else if (key > temp->key)
+            {
+                temp = temp->right;
+            }
+            else
+            {
+                return true; // means duplicate is found
+            }
+        }
+        return false; // hamein key nahi mili
     }
 };
