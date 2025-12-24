5963f288d544a4cc754d00a45d2e5f7824f0763e
mujeebarshad007 <mujeeb.arshad001@gmail.com>
2025-12-24 11:01:22 +0500

    DFS

--- GRAPHS/client.cpp
+++ GRAPHS/client.cpp
@@ -1,98 +1,27 @@
-#pragma once
-#include <forward_list>
-template <typename V, typename E>
-class graph
+#include <iostream>
+#include "graph.h"
+int main()
 {
+    graph<char, int> g;
+    g.add_vertex('A');
+    g.add_vertex('B');
+    g.add_vertex('C');
+    g.add_vertex('D');
 
-    std::forward_list<std::pair<int, E>> *edges;
-    int n;
-    int g_size;
-    V *vertices;
-    int get_index(const V &v)
-    {
-        for (int i = 0; i < n; ++i)
-        {
-            if (vertices[i] == v)
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
+    g.add_edge('A', 'C', 2);
+    g.add_edge('B', 'D', 3);
+    g.add_edge('C', 'B', 5);
+    g.add_edge('D', 'B', 6);
+    g.add_edge('D', 'A', 1);
 
-public:
-    graph(int s = 100)
+    std::cout << g.weight('A', 'C') << std::endl;
+    std::forward_list<char> neighbors;
+    neighbors = g.neighbors('D');
+    auto it = neighbors.begin();
+    std::cout << "Neighbors of D: ";
+    while (it != neighbors.end())
     {
-        g_size = s;
-        n = 0;
-        edges = new std::forward_list<std::pair<int, E>>[s];
-        vertices = new V[g_size];
+        std::cout << *it << " ";
+        ++it;
     }
-
-    ~graph()
-    {
-        delete[] edges;
-        delete[] vertices;
-    }
-
-    void add_vertex(const V &v)
-    {
-        vertices[n] = v;
-        ++n;
-    }
-
-    void add_edge(const V &s, const V &d, const E &w)
-    {
-        int s_index, d_index;
-        s_index = get_index(s);
-        d_index = get_index(d);
-
-        edges[s_index].push_front({d_index, w});
-    }
-
-    void make_Empty()
-    {
-        for (int i = 0; i < n; ++i)
-        {
-            edges[i].clear();
-            n = 0;
-        }
-    }
-    bool is_empty() const
-    {
-        return n == 0;
-    }
-    bool is_full() const
-    {
-        return n == g_size;
-    }
-    E weight(const V &s, const V &d)
-    {
-        int s_index, d_index;
-        s_index = get_index(s);
-        d_index = get_index(d);
-        auto it = edges[s_index].begin();
-        while (it != edges[s_index].end())
-        {
-            if (it->first == d_index)
-            {
-                return it->second;
-                ++it;
-            }
-        }
-    }
-
-    std::forward_list<V> neighbors(const V &v)
-    {
-        int v_index;
-        v_index = get_index(v);
-        std::forward_list<V> f;
-        auto it = edges[v_index].begin();
-        while (it != edges[v_index].end())
-        {
-            f.push_front(vertices[it->first]);
-            ++it;
-        }
-        return;
-    }
-};
+}
---
+++ GRAPHS/graph.h
@@ -0,0 +1,133 @@
+#pragma once
+#include <forward_list>
+#include <stack>
+// Directed graph
+// Weighted graph
+template <typename V, typename E>
+class graph
+{
+private:
+    std::forward_list<std::pair<int, E>> *edges;
+    V *vertices;
+    int g_size;
+    int n;
+    void grow_graph(int new_size)
+    {
+    }
+    int get_index(const V &v) const
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            if (vertices[i] == v)
+                return i;
+        }
+        return -1;
+    }
+
+public:
+    graph(int S = 100)
+    {
+        g_size = S;
+        n = 0;
+        edges = new std::forward_list<std::pair<int, E>>[S];
+        vertices = new V[S];
+    }
+    ~graph()
+    {
+        delete[] vertices;
+        delete[] edges;
+    }
+    void add_vertex(const V &v)
+    {
+        vertices[n] = v;
+        ++n;
+    }
+    void add_edge(const V &s, const V &d, const E &w)
+    {
+        int s_i, d_i;
+        s_i = get_index(s);
+        d_i = get_index(d);
+        if (s_i == -1 || d_i == -1)
+            throw("Invalid vertex");
+        edges[s_i].push_front({d_i, w});
+    }
+    void clear()
+    {
+        /*
+        delete[]vertices;
+        delete[]edges;
+        edges = new forward_list<std::pair<int, E>>[g_size];
+        vertices = new V[g_size];
+        */
+
+        for (int i = 0; i < n; ++i)
+            edges[i].clear();
+        n = 0;
+    }
+
+    bool is_empty() const
+    {
+        return n == 0;
+    }
+    bool is_full() const
+    {
+        return g_size == n;
+    }
+    E weight(const V &s, const V &d) const
+    {
+        int s_i, d_i;
+        s_i = get_index(s);
+        d_i = get_index(d);
+        auto it = edges[s_i].begin();
+        while (it != edges[s_i].end())
+        {
+            if (it->first == d_i)
+                return it->second;
+            ++it;
+        }
+        throw("Edge does not exist");
+    }
+    std::forward_list<V> neighbors(const V &v) const
+    {
+        int v_i;
+        v_i = get_index(v);
+        std::forward_list<V> f;
+        auto it = edges[v_i].begin();
+        while (it != edges[v_i].end())
+        {
+            f.push_front(vertices[it->first]);
+            ++it;
+        }
+        return f;
+    }
+
+    bool search_DFS(const V &source, const V &destination)
+    {
+        std::stack<V> s;
+        bool *marked;
+        marked = new bool[n];
+        marked = {true};
+
+        int source_index = get_index(source);
+        int destination_index = get_index(destination);
+
+        s.push(source_index);
+
+        while (!s.empty())
+        {
+            int a = s.top();
+            s.pop();
+            if (a == destination_index)
+                return true;
+
+            if (marked[a])
+            {
+            }
+            std::forward_list<V> f = neighbors(a);
+            while (!f.empty())
+            {
+                s.push(f.front());
+            }
+        }
+    }
+};
